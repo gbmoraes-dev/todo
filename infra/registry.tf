43fa2d1cd967ff4c005c3857a9ffea3829405138
gbmoraes-dev <dev.gbmoraes@gmail.com>
2026-05-28 22:38:31 -0300

    ci: change image path

--- infra/registry.tf
+++ infra/registry.tf
@@ -3,24 +3,25 @@ resource "google_project_service" "artifactregistry" {
   disable_on_destroy = false
 }
 
-resource "google_artifact_registry_repository" "dockerhub_proxy" {
+resource "google_artifact_registry_repository" "todo" {
   location      = var.region
-  repository_id = "dockerhub"
+  repository_id = "todo"
   format        = "DOCKER"
-  mode          = "REMOTE_REPOSITORY"
-
-  remote_repository_config {
-    docker_repository {
-      public_repository = "DOCKER_HUB"
-    }
-  }
+  mode          = "STANDARD_REPOSITORY"
 
   depends_on = [google_project_service.artifactregistry]
 }
 
 resource "google_artifact_registry_repository_iam_member" "github_actions_push" {
-  location   = google_artifact_registry_repository.dockerhub_proxy.location
-  repository = google_artifact_registry_repository.dockerhub_proxy.name
+  location   = google_artifact_registry_repository.todo.location
+  repository = google_artifact_registry_repository.todo.name
   role       = "roles/artifactregistry.writer"
   member     = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+resource "google_artifact_registry_repository_iam_member" "compute_pull" {
+  location   = google_artifact_registry_repository.todo.location
+  repository = google_artifact_registry_repository.todo.name
+  role       = "roles/artifactregistry.reader"
+  member     = "serviceAccount:${var.project_number}-compute@developer.gserviceaccount.com"
 }
